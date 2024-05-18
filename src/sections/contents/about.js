2d010db26c71d5eab56b19242ef3f753d6baa411
Kittabun Sukkasem <>
2024-05-19 00:02:10 +0700

    #feat: update

--- src/sections/contents/about.js
+++ src/sections/contents/about.js
@@ -1,4 +1,4 @@
 export const data = {
     title: "About Me",
-    descrioption: "In my current position at nex unify co., ltd, I serve as a dedicated Flutter Mobile Developer. I lead and contribute to projects that leverage Flutter to create seamless, cross-platform mobile applications. Through this role, I've gained valuable insights into the intricacies of mobile development, ensuring that the apps I work on are not only visually appealing but also performant and scalable."
+    descrioption: "I serve as a dedicated Flutter Mobile Developer. I lead and contribute to projects that leverage Flutter to create seamless, cross-platform mobile applications. Through this role, I've gained valuable insights into the intricacies of mobile development, ensuring that the apps I work on are not only visually appealing but also performant and scalable."
 }
